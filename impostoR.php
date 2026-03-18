e359e4a4f6f4438694b5b21d9848d4e1d6eec565
josoares-png <joo.soaress32@gmail.com>
2026-03-17 22:17:45 -0300

    atividade imposto de renda

---
+++ impostoR.php
@@ -0,0 +1,22 @@
+<?php 
+   
+  
+   echo "Imposto de renda: <br/> ";
+   echo "Porcentagem de desconto: <br/>";
+   echo "Valor de desconto: <br/>";
+   echo "Salário líquido: <br/>";
+
+   $salL;
+   $salB; 
+   $porc;
+   
+   echo "Salário bruto: <br/>";
+
+   if($salB <= 2428.80){
+   	echo "Isento";
+   }
+   	salB-(salB * 0,0075 / 100)
+   	echo"Valor de desconto: <br/>";
+   }
+
+ ?>
