bad299ae24307075660dca5414f0c480990ba5b3
josoares-png <joo.soaress32@gmail.com>
2026-03-24 15:47:56 -0300

    atividade imposto

--- impostoR.php
+++ impostoR.php
@@ -1,22 +1,32 @@
 <?php 
    
   
-   echo "Imposto de renda: <br/> ";
+  /* echo "Imposto de renda: <br/> ";
    echo "Porcentagem de desconto: <br/>";
    echo "Valor de desconto: <br/>";
-   echo "Salário líquido: <br/>";
+   echo "Salário líquido: <br/>"; */
 
-   $salL;
-   $salB; 
-   $porc;
+
+   $salB = 2800.00; 
    
-   echo "Salário bruto: <br/>";
+   
+   echo "Salário bruto: $salB <br/><br/>";
+
+   echo "Tabela de Imposto de Renda<br/><br/>";
+
+
 
    if($salB <= 2428.80){
-   	echo "Isento";
-   }
-   	salB-(salB * 0,0075 / 100)
-   	echo"Valor de desconto: <br/>";
-   }
+   	echo "Isento <br/>";
+   }else if($salB >= 2428.81 && $salB <= 2826.65){
+      $porc =   0.075;
+      $desc = $salB * $porc;
+      $salL = $salB - $desc;
+   }else if()
+      echo "Porcentagem de desconto: " .($porc * 100). "% <br/>";
+      echo"Valor de desconto: R$ $desc <br/>";
+      echo "Salário Líquido: R$ $salL";
+
+   
 
  ?>
